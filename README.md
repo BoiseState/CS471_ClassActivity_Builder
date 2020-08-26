e6c3618cefba52f82532062983d80462181e04ca
Bogdan Dit <bogdandit@boisestate.edu>
2020-08-26 12:13:02 -0600

    Initial commit but it's not compiling...

---
+++ README.md
@@ -0,0 +1,7 @@
+# CS471 Class Activity: Builder
+
+Without making any changes to the code in the [src/](src/) folder, 
+how can you make the code compile and run successfully?
+
+## Eclipse Project
+This repository is an [Eclipse](https://www.eclipse.org/) project.
---
+++ src/main/java/edu/boisestate/cs/cs471classexercise/builder/MainApp.java
@@ -0,0 +1,21 @@
+package edu.boisestate.cs.cs471classexercise.builder;
+
+import com.google.gson.Gson;
+
+/**
+ * Hello world!
+ */
+public class MainApp
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        
+        //we want to use Google's JSON library for Java called Gson
+        Gson gson = new Gson();
+
+
+
+    }
+
+}
